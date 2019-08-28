ba0f2d28ef96175c184863809005ced1ffe825be
Piers Chambers <piers@varyonic.com>
2019-08-28 16:21:01 -0400

    bundle gem active_merchant-flex_pay

---
+++ lib/active_merchant/flex_pay.rb
@@ -0,0 +1,8 @@
+require "active_merchant/flex_pay/version"
+
+module ActiveMerchant
+  module FlexPay
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/active_merchant/flex_pay/version.rb
@@ -0,0 +1,5 @@
+module ActiveMerchant
+  module FlexPay
+    VERSION = "0.1.0"
+  end
+end
