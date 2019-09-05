4b8958d9590da65d6ef7f0f32554859c51dcd2bd
Piers Chambers <piers@varyonic.com>
2019-09-05 14:35:54 -0400

    Remove unused gem template code.

--- lib/active_merchant/flex_pay.rb
+++ lib/active_merchant/flex_pay.rb
@@ -1,10 +1,3 @@
 require "active_merchant"
 require "active_merchant/flex_pay/version"
 require "active_merchant/billing/gateways/flex_pay"
-
-module ActiveMerchant
-  module FlexPay
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
